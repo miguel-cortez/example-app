cd28a1a6b45846d004ab5a7a79a7f38784860fc1
Miguel Cortez <mcortez_vasquez@yahoo.com>
2025-06-02 16:53:21 -0600

    Protección de rutas - proceso iniciado

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -5,14 +5,41 @@ import ProdsCat from '../components/GraficoTotalProductosCategorias.vue';
 import PreviewPdf from '../components/PreviewPdfFileComponent.vue';
 const routes = [
     { path: '/dashboard/home', component: Home },
-    { path: '/dashboard/productos', component: Prod },
+    { path: '/dashboard/productos', component: Prod, meta: { requiresAuth: true } },
     { path: '/dashboard/prods_cat', component: ProdsCat },
-    { path: '/dashboard/preview_pdf', component: PreviewPdf }
+    { path: 
+        '/dashboard/preview_pdf',
+         component: PreviewPdf,
+        meta: { requiresAuth: true },
+     }
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
+const loggedIn = () =>{
+    return true;
+}
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if (!loggedIn()) {
+      next({
+        path: '/login',
+        //query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next() // make sure to always call next()!
+  }
+})
+
+
 
 export default router;
