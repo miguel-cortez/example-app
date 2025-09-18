3ee2bdea24d407bb9560f8687c673c088f042189
Miguel Cortez <mcortez_vasquez@yahoo.com>
2025-09-18 08:27:01 -0600

    Nuevos cambios para deployments

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,51 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
+
+
+//////////////////////
+// para deploy
+
+import { createApp } from 'vue';
+import App from './components/App.vue';
+import router from './router';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import Button from "primevue/button"
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+import 'primeicons/primeicons.css';
+import axios from 'axios'
+import Tooltip from 'primevue/tooltip';
+
+const app = createApp(App);
+
+app.config.globalProperties.$axios = axios;
+window.axios = axios;
+
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.use(ToastService);
+
+app.component('Button', Button);
+app.component('Toast', Toast);
+app.directive('tooltip', Tooltip);
+window.Vue = app;
+app.mount('#app');
+
+
+
+
+
+////////////////////////
+
+
+
+
+
+
 Alpine.start();
